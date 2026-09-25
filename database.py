368fefdeff00d3ec0d5028943a46a14c9892cb6b
CGH4153 <145848241+CGH4153@users.noreply.github.com>
2026-09-25 14:02:07 +0200

    registro de usuario

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+import hashlib
 
 def base_de_datos():
     conexion = sqlite3.connect("datos.db")
@@ -43,5 +45,37 @@ def base_de_datos():
     conexion.commit()
     conexion.close()
 
+def registrar_usuario(username, password):
+    # SALT ALEATORIO
+    salt = os.urandom(16)
+
+    # PBKDF2-HMAC-SHA256
+    password_hash = hashlib.pbkdf2_hmac(
+        'sha256',
+        password.encode('utf-8'),
+        salt,
+        100000
+    )
+
+    # CONVERTIR A HEXADECIMAL
+    salt_hex = salt.hex()
+    hash_hex = password_hash.hex()
+
+    # AÑADIR A SQLITE
+    conexion = sqlite3.connect("datos.db")
+    cursor = conexion.cursor()
+    try:
+        cursor.execute(
+            "INSERT INTO users (username, password, salt) VALUES (?, ?, ?)",
+            (username, hash_hex, salt_hex)
+        )
+        conexion.commit()
+        print(f"Usuario {username} registrado con éxito.")
+    except sqlite3.IntegrityError:
+        print("Error: El usuario ya existe.")
+    finally:
+        conexion.close()
+
 if __name__ == "__main__":
     base_de_datos()
+    registrar_usuario("byak", "22112005")
